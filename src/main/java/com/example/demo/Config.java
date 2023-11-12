2b6bef94669139d2a74fc2fe57ff4f2ba11bdf16
Wei Li <weili.mail@gmail.com>
2023-11-12 11:30:15 -0500

    Add BeanA via Config class using a different bean name

---
+++ src/main/java/com/example/demo/Config.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    BeanA beanAFromConfig() {
+        System.out.println("================================================");
+        System.out.println("run beanA @Configuration");
+        System.out.println("================================================");
+        return new BeanA();
+    }
+    
+}
