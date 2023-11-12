36c5eceb5716d31850261732508bf7737b458d95
Wei Li <weili.mail@gmail.com>
2023-11-12 11:18:19 -0500

    Add BeanA via @Componment

---
+++ src/main/java/com/example/demo/BeanA.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class BeanA {
+
+    @PostConstruct
+    void postConstruct() {
+        System.out.println("================================================");
+        System.out.println("run postConstruct BeanA");
+        System.out.println("================================================");
+    }
+    
+}
---
+++ src/main/java/com/example/demo/FirstImportSelector.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class FirstImportSelector implements ImportSelector {
+
+      @Override
+      public String[] selectImports (AnnotationMetadata importingClassMetadata) {
+        System.out.println("================================================");
+        System.out.println("run FirstImportSelector");
+        System.out.println("================================================");
+        return new String[0]; 
+      }
+}
